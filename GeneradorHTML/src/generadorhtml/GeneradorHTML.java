bd687f45715e98eff4dcdcb8b17dca3a2876b491
spartan0614 <62442joss@gmail.com>
2019-03-15 23:41:33 -0600

    Almacenamiento de objetos

--- GeneradorHTML/src/generadorhtml/GeneradorHTML.java
+++ GeneradorHTML/src/generadorhtml/GeneradorHTML.java
@@ -35,9 +35,6 @@ public class GeneradorHTML {
         
 //        CrearHTML pt = new CrearHTML();
 //        pt.setVisible(true);
-        
-        boolean a = true != false;
-                
     }
     
     public static void generarLexico(String path){
--- GeneradorHTML/src/rootcode/Objeto.java
+++ GeneradorHTML/src/rootcode/Objeto.java
@@ -3,12 +3,12 @@ package rootcode;
 /*
   @author Dinora
  */
-public class Variable {
+public class Objeto {
     public String tipo;
     public String nombre;
     public String valor;
-     
-    public Variable(String tipo, String nombre, String valor){
+    
+    public Objeto(String tipo, String nombre, String valor){
         this.tipo = tipo;
         this.nombre = nombre;
         this.valor = valor;
